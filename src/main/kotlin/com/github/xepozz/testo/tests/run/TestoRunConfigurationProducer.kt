6ccabd9c09d7a32f908277b994f788784837c898
Dmitriy Derepko <xepozz@list.ru>
2025-10-26 15:53:28 +0400

    fix: method detection

--- src/main/kotlin/com/github/xepozz/testo/tests/run/TestoRunConfigurationProducer.kt
+++ src/main/kotlin/com/github/xepozz/testo/tests/run/TestoRunConfigurationProducer.kt
@@ -1,21 +1,16 @@
 package com.github.xepozz.testo.tests.run
 
-import com.github.xepozz.testo.TestoClasses
+import com.github.xepozz.testo.isTesto
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Condition
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiNamedElement
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.util.elementType
 import com.intellij.util.asSafely
-import com.jetbrains.php.lang.lexer.PhpTokenTypes
-import com.jetbrains.php.lang.parser.PhpElementTypes
 import com.jetbrains.php.lang.psi.PhpFile
 import com.jetbrains.php.lang.psi.elements.Function
-import com.jetbrains.php.lang.psi.elements.Method
 import com.jetbrains.php.lang.psi.elements.PhpClass
 import com.jetbrains.php.phpunit.PhpUnitTestDescriptor
 import com.jetbrains.php.testFramework.run.PhpTestConfigurationProducer
@@ -39,10 +34,7 @@ class TestoRunConfigurationProducer : PhpTestConfigurationProducer<TestoRunConfi
     override fun getConfigurationFactory() = TestoRunConfigurationFactory(TestoRunConfigurationType.INSTANCE)
 
     companion object Companion {
-        val METHOD = Condition<PsiElement> {
-            (it.elementType == PhpTokenTypes.IDENTIFIER)
-                    && (it.parent is Function && (it.parent as Function).getAttributes(TestoClasses.TEST).isNotEmpty())
-        }
+        val METHOD = Condition<PsiElement> { it.isTesto() }
         private val METHOD_NAMER = { element: PsiElement? -> (element as? Function)?.name }
         private val FILE_TO_SCOPE = { file: PsiFile? ->
             println("file to scope: ${file?.virtualFile?.name}")
