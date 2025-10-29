7b73e7d9228ff2b0e84f5a1908bb53c7ec54899c
Dmitriy Derepko <xepozz@list.ru>
2025-10-29 09:08:54 +0400

    feat: add composer config

---
+++ src/main/kotlin/com/github/xepozz/testo/TestoComposerConfig.kt
@@ -0,0 +1,16 @@
+package com.github.xepozz.testo
+
+import com.github.xepozz.testo.tests.TestoFrameworkType
+import com.github.xepozz.testo.tests.run.TestoRunConfigurationType
+import com.jetbrains.php.testFramework.PhpTestFrameworkComposerConfig
+
+class TestoComposerConfig : PhpTestFrameworkComposerConfig(TestoFrameworkType.INSTANCE, PACKAGE, RELATIVE_PATH) {
+    override fun getDefaultConfigName() = "phpunit.xml"
+
+    override fun getConfigurationType() = TestoRunConfigurationType.INSTANCE
+
+    companion object Companion {
+        private const val PACKAGE = "testo/testo"
+        private const val RELATIVE_PATH = "bin/testo"
+    }
+}
