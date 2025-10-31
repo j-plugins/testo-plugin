489a1d0bd8a04e034a4fa0b5d9a41b9348cdb853
Dmitriy Derepko <xepozz@list.ru>
2025-10-31 21:10:00 +0400

    fix: allow configuration to replace others

--- src/main/kotlin/com/github/xepozz/testo/tests/run/TestoRunConfigurationProducer.kt
+++ src/main/kotlin/com/github/xepozz/testo/tests/run/TestoRunConfigurationProducer.kt
@@ -4,20 +4,16 @@ import com.github.xepozz.testo.index.TestoDataProviderUtils
 import com.github.xepozz.testo.isTestoExecutable
 import com.github.xepozz.testo.isTestoFile
 import com.intellij.execution.Location
-import com.intellij.execution.PsiLocation
-import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.ConfigurationFromContext
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Condition
-import com.intellij.openapi.util.Ref
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.util.PsiUtilCore
 import com.jetbrains.php.lang.psi.elements.Function
 import com.jetbrains.php.lang.psi.elements.Method
 import com.jetbrains.php.phpunit.PhpMethodLocation
-import com.jetbrains.php.run.PhpRunConfigurationExtensionsManager
 import com.jetbrains.php.testFramework.run.PhpTestConfigurationProducer
 import com.jetbrains.php.testFramework.run.PhpTestRunnerSettings
 
@@ -35,8 +31,6 @@ class TestoRunConfigurationProducer : PhpTestConfigurationProducer<TestoRunConfi
         element: PsiElement,
         virtualFile: VirtualFile
     ): PsiElement? {
-        val testRunnerSettings = testRunnerSettings as? TestoRunnerSettings ?: return null
-
         if (element is Method) {
             val element = findTestElement(element, getWorkingDirectory(element))
             if (element is Method) {
@@ -54,10 +48,10 @@ class TestoRunConfigurationProducer : PhpTestConfigurationProducer<TestoRunConfi
 
     override fun isConfigurationFromContext(
         testRunnerSettings: PhpTestRunnerSettings,
-        psiElement: PsiElement
+        element: PsiElement
     ): Boolean {
-        if (psiElement is Method) {
-            val usages = TestoDataProviderUtils.findDataProviderUsages(psiElement)
+        if (element is Method) {
+            val usages = TestoDataProviderUtils.findDataProviderUsages(element)
 
             if (usages.isNotEmpty()) {
                 val target = usages.first()
@@ -70,7 +64,7 @@ class TestoRunConfigurationProducer : PhpTestConfigurationProducer<TestoRunConfi
                 }
             }
         }
-        return super.isConfigurationFromContext(testRunnerSettings, psiElement)
+        return super.isConfigurationFromContext(testRunnerSettings, element)
     }
 
     override fun getWorkingDirectory(element: PsiElement): VirtualFile? {
@@ -83,14 +77,17 @@ class TestoRunConfigurationProducer : PhpTestConfigurationProducer<TestoRunConfi
 
     override fun getConfigurationFactory() = TestoRunConfigurationFactory(TestoRunConfigurationType.INSTANCE)
 
+    override fun shouldReplace(self: ConfigurationFromContext, other: ConfigurationFromContext) = true
+
     companion object Companion {
         val METHOD = Condition<PsiElement> {
             it.isTestoExecutable() || (it is Method && TestoDataProviderUtils.isDataProvider(it))
         }
         private val METHOD_NAMER = { element: PsiElement? -> (element as? Function)?.name }
         private val FILE_TO_SCOPE = { file: PsiFile? ->
-            println("file to scope: ${file?.virtualFile?.name}")
-            file?.takeIf { it.isTestoFile() }
+            file
+                ?.takeIf { it.isTestoFile() }
+                .apply { println("file to scope: $file -> $this") }
         }
     }
 }
