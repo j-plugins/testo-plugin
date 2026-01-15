24ce41c53a165b685bc26f56bad49c626d1ac043
Dmitriy Derepko <xepozz@list.ru>
2026-01-15 11:39:17 +0400

    feat: add test action provider

---
+++ src/main/kotlin/com/github/xepozz/testo/tests/actions/TestoTestActionProvider.kt
@@ -0,0 +1,31 @@
+package com.github.xepozz.testo.tests.actions
+
+import com.github.xepozz.testo.TestoIcons
+import com.intellij.execution.testframework.TestConsoleProperties
+import com.intellij.execution.testframework.TestFrameworkRunningModel
+import com.intellij.execution.testframework.ToggleModelAction
+import com.intellij.execution.testframework.ToggleModelActionProvider
+import com.intellij.util.config.BooleanProperty
+
+class TestoTestActionProvider : ToggleModelActionProvider {
+    override fun createToggleModelAction(properties: TestConsoleProperties?): ToggleModelAction? {
+        // TODO: Implement this method
+        return TestoToggleModelAction(properties)
+    }
+}
+
+class TestoToggleModelAction(properties: TestConsoleProperties?) : ToggleModelAction(
+    "Testo Button",
+    "It does something",
+    TestoIcons.TESTO,
+    properties,
+    BooleanProperty("Something", false)
+) {
+    var myModel: TestFrameworkRunningModel? = null
+    
+    override fun setModel(model: TestFrameworkRunningModel?) {
+        this.myModel = model
+    }
+
+    override fun isEnabled(): Boolean = true
+}
